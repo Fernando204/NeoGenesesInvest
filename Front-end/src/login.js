2d8b556d72a318bba28a901005109fdfcac91a10
Fernando Amorim <fernadotb2@gmail.com>
2025-05-23 11:37:59 -0300

    security update

--- Front-end/src/login.js
+++ Front-end/src/login.js
@@ -1,51 +1,50 @@
 const bt = document.getElementById("enterBT");
-const errorLabel = document.getElementById("errorLabel");
-
 bt.addEventListener("click",()=>{
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-
+    // Regex simples para o email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Validações
     if (!email || !password) {
-        errorLabel.innerHTML = "Por favor preencha todos os campos";
+        alert("Por favor, preench todos os campos.");
+        return;
+    }
+    if (!emailRegex.test(email)) {
+        alert("O e-mail inserido não é válido.");
         return;
     }
 
+    if (password.length < 8) {
+        alert("A senha deve ter pelo menos 8 caracteres.");
+        return;
+    }
+
+    // Requisição
     fetch('http://localhost:8080/user/login',{
         method: "POST",
         headers: {"Content-Type": "application/json"},
-        credentials: "include",
+        credentials: "include", // essencial para aceitar cookies HttpOnly
         body: JSON.stringify({password: password,email: email})
     })
     .then(response =>{
         if(response.ok){
             return response.json();
-        }else{
-            return response.text().then(text=>{
-                errorLabel.innerHTML = ("erro: "+text);
-                console.log(text)
-            })
         }
-    }).then(data =>{
-        console.log(data);
-        
-        let dat = {user: null}
-        
-        const userDat = {
-            name: data.name,
-            id: data.id
+        return response.text().then(text=>{
+                throw new Error("Erro: "+text);
+        });
+    })                                                   
+    .then(data =>{
+        if (data && data.name && data.id) {
+            alert(`${data.name} logado com sucesso`); 
+            location.href = "index.html";
+        } else {
+            console.error("Resposta inesperada:", data);
+            alert("Erro inesperado ao fazer login");
         }
-        dat.user = userDat;
-        
-        alert(data.name+" logado com sucesso");
-        location.href = "index.html"
     })
-    .catch(error =>{
-        console.log(error);
-    })
-    
-   
+    .catch(error => { 
+        alert("Ocorreu um erro ao tentar fazer login. Tente novamente mais tarde.");
+        console.error("Erro no login:", error);
+    });
 })
-//hello
-// word
-// when day is gone
-// today i will eat your ass with rice and beans ;
